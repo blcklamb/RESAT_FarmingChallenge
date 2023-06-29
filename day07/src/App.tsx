f3e466831a30a688326993533a3aafbe26940062
blcklamb <whenucan35@gmail.com>
2023-06-29 19:21:12 +0900

    add: basic setting - app, index, footer, header

--- day07/src/App.tsx
+++ day07/src/App.tsx
@@ -1,5 +1,15 @@
+import SVGLoader from "./assets/SVGLoader";
+import Header from "./component/Header.tsx";
+import { GlobalStyle } from "./style/global-style.ts";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <GlobalStyle></GlobalStyle>
+      <SVGLoader />
+      <Header />
+    </div>
+  );
 }
 
 export default App;
