4667ecb226e0b98d728da3b21cdbf94077af0b01
blcklamb <whenucan35@gmail.com>
2023-07-01 01:59:33 +0900

    add: complete signin feature - style: layout, validate control - header: signin control - component: form, label, notification

--- day10/src/App.tsx
+++ day10/src/App.tsx
@@ -1,5 +1,18 @@
+import { useState } from "react";
+import Header from "./component/Header";
+import SignIn from "./component/SignIn";
+
 function App() {
-  return <div className="App"></div>;
+  const [signInCompleted, setSignInCompleted] = useState<boolean>(false);
+  const onCompleteSignIn = () => {
+    setSignInCompleted(true);
+  };
+  return (
+    <div className="App">
+      <Header isSignedIn={signInCompleted} />
+      <SignIn onCompleteSignIn={onCompleteSignIn} />
+    </div>
+  );
 }
 
 export default App;
